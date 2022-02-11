5754bf8d6f3fbf81bfe985e54153481394a10cd2
paul-serafimescu <paulserafimescu@gmail.com>
2022-02-10 19:50:32 -0800

    network basics & dataset

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,35 +1,43 @@
 import torch
-import torchvision
-import pandas as pd
 import os
+import pandas as pd
+from pathlib import Path
+from PIL import Image
+from torchvision import transforms
 
 class StartingDataset(torch.utils.data.Dataset):
-    """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
-    """
+
+    # spits out image tensors which resnet18 should like
 
     def __init__(self, transform = None, target_transform = None):
+        self.root_dir = Path.cwd()
+
         # Read csv into labels
-        self.labels = pd.read_csv('./cassava-leaf-disease-classification/train.csv')
+        self.labels = pd.read_csv(os.path.join(self.root_dir, 'data', 'train.csv'))
         
         # Image directory path
-        self.img_dir = './cassava-leaf-disease-classification/train_images'
+        self.img_dir = os.path.join(self.root_dir, 'cassava-leaf-classification', 'train_images')
 
         self.transform, self.target_transform = transform, target_transform
 
     def __getitem__(self, index):
-        # Read the image into a tensor
-        image = torchvision.io.read_image(os.path.join(self.img_dir, self.labels.iloc[index]['image_id'])).float()
+        # Read the image into a tensor dim 224 x 224 x 3
+        image = Image.open(os.path.join(self.img_dir, self.labels.iloc[index]['image_id']))
         
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        input_batch = preprocess(image)
     
-        # Do the transforms if they exist
-        if self.transform is not None:
-            image = self.transform(image)
         label = self.labels.iloc[index]['label']
-        if self.target_transform is not None:
-            label = self.transform(label)
         
-        return image, label
+        return input_batch, label
 
     def __len__(self):
         return len(self.labels)
+
+# print(StartingDataset()[0])
--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -3,17 +3,20 @@ import torch.nn as nn
 
 class StartingNetwork(torch.nn.Module):
     """
-    Basic logistic regression on 600x800x3 images.
+    Basic logistic regression on 224 x 224 x 3 images.
     """
 
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.fc = nn.Linear(600 * 800 * 3, 5)
-        self.sigmoid = nn.Sigmoid()
+        self.resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained = True)
+        self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1])) # output dimension 512
+        
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = nn.Linear(512, 5)
 
     def forward(self, x):
-        x = self.flatten(x)
-        x = self.fc(x)
-        x = self.sigmoid(x)
+        x = self.resnet(x)
+        x = self.fc1(x)
+        x = torch.squeeze(x)
+        x = self.fc2(x)
         return x
